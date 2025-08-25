3772aa382e143aa0aab411930cf86bf16aaa5221
ruanvdw <ruanvanderwesthuizen0@gmail.com>
2025-08-25 23:46:04 +0200

    • Bootsrap fixes. • Removed unneccecary files

--- js/script.js
+++ js/script.js
@@ -1,4 +1,5 @@
-// Add a click event to the button
+/*// Add a click event to the button
 document.getElementById("myButton").addEventListener("click", function() {
   document.getElementById("message").textContent = "You clicked the button! 🎉";
 });
+*/
